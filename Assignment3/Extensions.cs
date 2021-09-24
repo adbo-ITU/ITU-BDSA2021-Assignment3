b8cc7acccf36ba6d3e0852e0dc7783a67257bd9a
Adrian Borup <avborup@gmail.com>
2021-09-24 13:44:03 +0200

    Add solutions for Extension Methods exercises
    Co-authored-by: Joachim Borup <JoachimBorup@users.noreply.github.com>

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Text.RegularExpressions;
 
-namespace BDSA2020.Assignment02
+namespace BDSA2020.Assignment03
 {
     public static class Extensions
     {
+        public static bool IsSecure(this Uri uri) => uri.Scheme == "https";
+
+        public static int WordCount(this string input) => Regex.Matches(input, @"\p{L}+").Count;
     }
 }
