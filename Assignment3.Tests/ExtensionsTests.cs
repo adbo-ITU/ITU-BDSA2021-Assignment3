bda2667de972daf4e9cca00f094f326b72f57428
Adrian Borup <avborup@gmail.com>
2021-09-24 13:44:03 +0200

    Add solutions for Extension Methods exercises
    Co-authored-by: Joachim Borup <JoachimBorup@users.noreply.github.com>

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,14 +1,47 @@
 using System;
 using Xunit;
 
-namespace BDSA2020.Assignment02.Tests
+
+namespace BDSA2020.Assignment03.Tests
 {
     public class ExtensionsTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("http://info.cern.ch/hypertext/WWW/TheProject.html")]
+        [InlineData("http://go.com/")]
+        public void IsSecure_given_http_url_is_false(string uriString)
         {
+            var uri = new Uri(uriString);
+            Assert.False(uri.IsSecure());
+        }
 
+        [Theory]
+        [InlineData("https://www.google.com/search?client=firefox-b-d&q=the+answer+to+life+the+universe+and+everything")]
+        [InlineData("https://open.kattis.com/")]
+        public void IsSecure_given_https_url_is_true(string uriString)
+        {
+            var uri = new Uri(uriString);
+            Assert.True(uri.IsSecure());
+        }
+
+        [Theory]
+        [InlineData("wss://www.example.com/socketserver")]
+        [InlineData("wss://edu.test.dk")]
+        public void IsSecure_given_wss_url_is_false(string uriString)
+        {
+            var uri = new Uri(uriString);
+            Assert.False(uri.IsSecure());
+        }
+
+        [Theory]
+        [InlineData("Hey my dear TA, I hope you're have a nice day!", 12)]
+        [InlineData("wss://edu.test.dk", 4)]
+        [InlineData("", 0)]
+        [InlineData("I love deadlines. I like the whooshing sound they make as they fly by.", 14)]
+        public void WordCount_returns_number_of_words_in_string(string input, int expectedNumWords)
+        {
+            var numWords = input.WordCount();
+            Assert.Equal(expectedNumWords, numWords);
         }
     }
 }
