0739ad388316fbce750f9037ce87647abf9d1139
Adrian Borup <adrian@borup.com>
2021-09-26 13:17:28 +0200

    Solve LINQ part of exercises (#3)
    Co-authored-by: Joachim Borup <JoachimBorup@users.noreply.github.com>
    
    * Add wizards to dataset
    
    * Save progress
    
    * Add query tests
    
    * Implement linq 1
    
    * Implement linq 2
    
    * Implement linq 3
    
    * Test and implement linq 4

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,14 +1,81 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
 
 namespace BDSA2020.Assignment03.Tests
 {
     public class QueriesTests
     {
         [Fact]
-        public void Test1()
+        public void RowlingWizards_returns_harry_harry_dumbledore_and_moody()
         {
+            var withExtensions = Queries.GetRowlingWizards_WithExtensions();
+            var withLinq = Queries.GetRowlingWizards_WithVeryLinq();
 
+            var expectedNames = new[] { "Harry Potter", "Harry Potter", "Dumbledore", "Alastor Moody" };
+            Assert.Equal(expectedNames, withExtensions);
+            Assert.Equal(expectedNames, withLinq);
+        }
+
+        [Fact]
+        public void FirstSithLordYear_returns_1977()
+        {
+            var withExtensions = Queries.FirstSithLordYear_WithExtensions();
+            var withLinq = Queries.FirstSithLordYear_WithVeryLinq();
+
+            var expectedYear = 1977;
+            Assert.Equal(expectedYear, withExtensions);
+            Assert.Equal(expectedYear, withLinq);
+        }
+
+        [Fact]
+        public void HarryPotterUnique_returns_harry_dumbledore_moody_with_year()
+        {
+            var withExtensions = Queries.HarryPotterUnique_WithExtensions();
+            var withLinq = Queries.HarryPotterUnique_WithVeryLinq();
+
+            var expectedPairs = new (string, int?)[] {
+                ("Harry Potter", 1997),
+                ("Dumbledore", 1997),
+                ("Alastor Moody", 2005),
+            };
+            Assert.Equal(expectedPairs, withExtensions);
+            Assert.Equal(expectedPairs, withLinq);
+        }
+
+        [Fact]
+        public void NamesReverseByCreatorThenName_returns_correct()
+        {
+            var withExtensions = Queries.NamesReverseByCreatorThenName_WithExtensions();
+            var withLinq = Queries.NamesReverseByCreatorThenName_WithVeryLinq();
+
+            IEnumerable<string> expectedNames = new[] {
+                "Alex Russo",
+
+                "Doctor Strange",
+
+                "Tara",
+                "Willow",
+
+                "Merlin",
+
+                "Gandalf",
+                "Sauron",
+
+                "Alastor Moody",
+                "Dumbledore",
+                "Harry Potter",
+                "Harry Potter",
+
+                "Darth Maul",
+                "Darth Sidious",
+                "Darth Vader",
+                "Yoda",
+
+                "The White Witch",
+            };
+            Assert.Equal(expectedNames, withExtensions);
+            Assert.Equal(expectedNames, withLinq);
         }
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,75 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
 namespace BDSA2020.Assignment03
 {
     public class Queries
     {
+        public static IEnumerable<string> GetRowlingWizards_WithExtensions()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return wizards.Where(w => w.Creator == "J. K. Rowling").Select(w => w.Name);
+        }
+
+        public static IEnumerable<string> GetRowlingWizards_WithVeryLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return from w in wizards
+                   where w.Creator == "J. K. Rowling"
+                   select w.Name;
+        }
+
+        public static int? FirstSithLordYear_WithExtensions()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return wizards.Where(w => w.Name.StartsWith("Darth")).Select(w => w.Year).Min();
+        }
+
+        public static int? FirstSithLordYear_WithVeryLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+            var years = from w in wizards
+                        where w.Name.StartsWith("Darth")
+                        select w.Year;
+            return years.Min();
+        }
+
+        public static IEnumerable<(string, int?)> HarryPotterUnique_WithExtensions()
+        {
+            var wizards = Wizard.Wizards.Value;
+
+            return wizards
+                .Where(w => w.Medium.Contains("Harry Potter"))
+                .Select(w => (w.Name, w.Year))
+                .Distinct();
+        }
+
+        public static IEnumerable<(string, int?)> HarryPotterUnique_WithVeryLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+            var tuples = from w in wizards
+                         where w.Medium.Contains("Harry Potter")
+                         select (w.Name, w.Year);
+            return tuples.Distinct();
+        }
+
+        public static IEnumerable<string> NamesReverseByCreatorThenName_WithExtensions()
+        {
+            var wizards = Wizard.Wizards.Value;
+            return wizards
+                .OrderByDescending(w => w.Creator)
+                .ThenBy(w => w.Name)
+                .Select(w => w.Name);
+        }
+
+        public static IEnumerable<string> NamesReverseByCreatorThenName_WithVeryLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+
+            return from w in wizards
+                   orderby w.Creator descending, w.Name ascending
+                   select w.Name;
+        }
     }
 }
